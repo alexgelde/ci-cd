08e0ecefd5ba4737ee49a8772cda68092495f118
Ronald Tejada <agelde@alu.ucam.edu>
2024-06-21 18:59:43 +0200

    Hola Dani

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Saludo from './Saludo';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Saludo nombre="Dani" />
       </header>
     </div>
   );
